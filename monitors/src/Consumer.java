bc5acc7b8cbbddf2c3722b9f43e93caa5b9037ad
mjste <mj.stefanik@gmail.com>
2022-10-10 20:42:29 +0200

    renaming

---
+++ monitors/src/Consumer.java
@@ -0,0 +1,18 @@
+public class Consumer implements Runnable {
+    private Monitor monitor;
+
+    public Consumer(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                monitor.consume();
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+}
---
+++ monitors/src/Producer.java
@@ -0,0 +1,18 @@
+public class Producer implements Runnable {
+    private Monitor monitor;
+
+    public Producer(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                monitor.produce();
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+}
